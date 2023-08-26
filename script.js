6bac420112f76e8247a73850dd83d39e78dc90f3
Righteous1ice <hoplabradley@gmail.com>
2023-08-26 17:32:48 +0100

    grid templeate is now added from js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const craftGrid = document.getElementById("craft-grid");
 function createGrid(rows, cols){
     const gridElement = document.getElementById("grid");
 
-    gridElement.style.gridTemplateRows = `repeat${rows}, 30px`;
-    gridElement.style.gridTemplateColumns = `repeat${cols}, 30px`;
+    gridElement.style.gridTemplateRows = `repeat(${rows}, 30px)`;
+    gridElement.style.gridTemplateColumns = `repeat(${cols}, 30px)`;
 
     for (let i = 0; i < rows * cols; i++){
         const cells = document.createElement("div");
