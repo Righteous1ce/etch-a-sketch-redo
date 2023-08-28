77cb57b2f12dfa5c72fb073cbf9c7566fd50ec03
Righteous1ice <hoplabradley@gmail.com>
2023-08-28 20:54:02 +0100

    Finally got the modal to pop up and close

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-const craftGrid = document.getElementById("grid-popup"); 
+
+const openModalButton = document.querySelectorAll("[data-modal-target]");
+const closeModalButton = document.querySelectorAll("[data-close-modal]");
+const createGridButton = document.querySelectorAll("[data-create-grid]");
+const backdrop = document.getElementById("backdrop");
 const gridElement = document.getElementById("grid");
 
+
+
+
+
+openModalButton.forEach(button => {
+    button.addEventListener("click", () =>{
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+        console.log(openModalButton);
+
+    })
+})
+
+closeModalButton.forEach(button => {
+    button.addEventListener("click", () => {
+        const modal = button.closest(".modal")
+        closeModal(modal);
+    })
+})
+
+
+
+////////// OPEN & CLOSE THE MODAL \\\\\\\\\\
+
+function openModal(modal){
+    console.log(modal);
+    if(modal == null) return;
+    modal.classList.add("active");
+    backdrop.classList.add("active");
+}
+
+
+function closeModal(modal){
+    if(modal == null) return
+    modal.classList.remove("active");
+    backdrop.classList.remove("active");
+}
 
 
 
